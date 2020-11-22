ee370b417a208809cf7000fc163990e8a1feac23
louis mane <dennis.itua1@gmail.com>
2020-11-22 13:07:40 -0500

    updated counter and counters components

--- front-end/src/components/counter.jsx
+++ front-end/src/components/counter.jsx
@@ -1,39 +1,49 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: []
+  // };
 
+  // we no longer need to get the props from the constructor
+  // since we are now using an arrow function method which enables the props by default
   // constructor(){
   //   super()
-  //   this.handleIncreament = this.handleIncreament.bind(this);
+  //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
   // styles = {
   //   fontSize: 15,
   //   fontWeight: "bold"
   // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-  return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
-  }
-
-  handleIncreament = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  }
+  // return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+  // }
+  // handleIncrement = product => {
+  //   console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // }
 
   render() {
+    // console.log("props", this.props);
     return (
       <div>
         <span style={{fontSize:15, fontWeight:"bold"}} 
         className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={() => this.handleIncreament({ id: 1 })} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} 
+        className="btn btn-secondary btn-sm"
+        >Increment
+        </button> 
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} 
+        className="btn btn-danger btn-sm m-2"
+        >Delete
+        </button>
+        {/* <br />
         {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        {this.renderTags()} */}
 
       </div>
     );
@@ -41,18 +51,18 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count !== 0) ? "primary" : "warning";
+    classes += (this.props.counter.value !== 0) ? "primary" : "warning";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    // return count === 0 ? "Zero" : count;
-    if (count === 0){
-      return "Zero"
-    }else{
-      return count;
-    }
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+    // if (value === 0){
+    //   return "Zero"
+    // }else{
+    //   return value;
+    // }
   }
 }
 
