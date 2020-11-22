76ae60e17a7d14acf1df2dd70d5e6c745565633d
louis mane <dennis.itua1@gmail.com>
2020-11-22 16:41:34 -0500

    updated remove product handler

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -44,6 +44,15 @@ handleIncrement = counter => {
     this.setState({ counters });
 }
 
+handleDecrement = counter => {
+  console.log(counter);
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index] = { ...counter };
+  counters[index].value--;
+  this.setState({ counters });
+}
+
 handleDelete = counterId => {
     // console.log("Event Handler Called: ", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
@@ -59,6 +68,7 @@ handleDelete = counterId => {
           counters={this.state.counters}
           onReset={this.handleReset} 
           onIncrement={this.handleIncrement} 
+          onDecrement={this.handleDecrement} 
           onDelete={this.handleDelete}
       />
       </main>
--- front-end/src/components/counter.jsx
+++ front-end/src/components/counter.jsx
@@ -45,8 +45,12 @@ class Counter extends Component {
         <span style={{fontSize:15, fontWeight:"bold"}} 
         className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={() => this.props.onIncrement(this.props.counter)} 
-        className="btn btn-secondary btn-sm"
-        >Increment
+        className="btn btn-secondary btn-sm m-2"
+        >Add Product
+        </button> 
+        <button onClick={() => this.props.onDecrement(this.props.counter)} 
+        className="btn btn-secondary btn-sm m-2"
+        >Remove Product
         </button> 
         <button onClick={() => this.props.onDelete(this.props.counter.id)} 
         className="btn btn-danger btn-sm m-2"
@@ -68,7 +72,7 @@ class Counter extends Component {
 
   formatCount() {
     const { value } = this.props.counter;
-    return value === 0 ? "Zero" : value;
+    return value === 0 ? "Product" : value;
     // if (value === 0){
     //   return "Zero"
     // }else{
